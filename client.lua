d1a05a85c85255ce6d356a20c58e55b0576ffb64
Ameer Mahdi <121252716+e60m@users.noreply.github.com>
2025-07-04 22:54:21 +0300

    Summer
    s

--- client.lua
+++ client.lua
@@ -1,6 +1,16 @@
 -- BlackMarket ESX Client Side
 ESX = exports['es_extended']:getSharedObject()
 
+
+RegisterCommand('testMessageVue', function()
+    exports.fd_laptop:sendAppMessage('fd_laptop_vue_template', {
+        action = "test",
+        data = {
+            test = "test"
+        }
+    })
+end, false)
+
 -- Configuration
 local Config = {
     -- Access locations (where players can access the black market)
